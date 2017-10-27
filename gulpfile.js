2a5f4217476e24232d499c837e9e773ea63591fd
lianhaiyang <574857444@qq.com>
2017-10-27 18:22:06 +0800

    添加样式，模板页面

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,46 @@
 /**
- * Created by nocoolyoyo on 2017/10/12.
- * gulp配置，webpack需要gulp搭配来弥补打包中的不足，（只是我目前webpack挖掘的不够深）
+ * Created by nocoolyoyo on 2017/10/26.
+ * gulp配置
  */
 
-let gulp = require('gulp');
-let concat = require('gulp-concat');
-let watchPath = './src/page/frame/';
+const gulp = require('gulp');
 
-import makeEntry from "./makeEntry"
+const sass = require('gulp-sass');
+const connect = require('gulp-connect');
+const plumber = require('gulp-plumber');
+
+const entryPath = './src/assets/scss/';
+const outputPath = './src/modules/custom/onemtSkin/';
 
 
-//目录配置生成
-gulp.task('build', function(){
-    makeEntry();
+//服务器
+gulp.task('webserver', function() {
+    connect.server({
+        root: './src/',
+        livereload: {
+            port:8070
+        },
+        port: 8070,
+        index: './src/pages/index.html'
+    });
 });
 //文件监听
 gulp.task('watch', function(){
-    gulp.watch(watchPath, ['build']);
+    gulp.watch(`${entryPath}*.scss`, ['build']);
+    gulp.watch(`${entryPath}components/*.scss`, ['build']);
     // Other watchers
 });
+
+//输出最终编译版本stylesheet文件
+gulp.task('build', function(){
+    return gulp.src(`${entryPath}style.scss`)
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(sass()) // Using gulp-sass
+        .pipe(plumber.stop())
+        .pipe(gulp.dest(outputPath))
+        .pipe(connect.reload());
+});
+
 // // 输出全部文章文件
-gulp.task('start', ['build',"watch"]);
+gulp.task('sass', ['build', 'watch', 'webserver']);
